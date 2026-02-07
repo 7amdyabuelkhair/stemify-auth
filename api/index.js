ac95f4025eb8b4be52c2ae902c849311c2a3cb0a
Hamdy Abu El-Khair <hamdyabuelkhair@gmail.com>
2026-02-07 16:47:42 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -37,13 +37,15 @@ function createToken(userId) {
 }
 
 module.exports = async (req, res) => {
-  // Enable CORS
+  // Enable CORS for all origins (including file:// and localhost)
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-admin-key');
+  res.setHeader('Access-Control-Max-Age', '86400'); // 24 hours
 
+  // Handle preflight requests
   if (req.method === 'OPTIONS') {
-    return res.status(200).end();
+    return res.status(200).json({});
   }
 
   const { method } = req;
